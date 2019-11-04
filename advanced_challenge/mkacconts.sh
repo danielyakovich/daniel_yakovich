69b619c1ad417d63ce75d652ebad8f57e1a5300a
root <root@localhost.localdomain>
2019-11-04 17:15:31 +0000

    mended the mkacconts.sh

--- advanced_challenge/mkacconts.sh
+++ advanced_challenge/mkacconts.sh
@@ -1,10 +1,11 @@
 #!/bin/bash 
 
-file="/newusers/newusers"
+file="./newusers"
 while IFS= read -r line; do
   first_name=$(echo $line | cut -d " " -f 1)
   last_name=$(echo $line | cut -d " " -f 2)
   user_name=$(echo ${first_name,,} | cut -b 1)${last_name,,}
-  useradd "$user_name"
-done <"$file"
+  sudo useradd "$user_name"
+  echo "made new user $user_name"
+done<"$file"
 
