aa1ec17be37715251be901ef28f1e17e133accda
root <root@localhost.localdomain>
2019-11-04 14:12:03 +0000

    advanced challenge folder commit

---
+++ advanced_challenge/mkacconts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+file="/tmp/advanced_challenge/newusers"
+while IFS= read -r line; do
+  first_name=$(echo $line | cut -d " " -f 1)
+  last_name=$(echo $line | cut -d " " -f 2)
+  user_name=$(echo ${first_name,,} | cut -b 1)${last_name,,}
+  useradd "$user_name"
+done <"$file"
+
